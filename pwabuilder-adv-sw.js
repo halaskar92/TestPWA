ab1b361cb9cb6f529b24406b4d667e51d8908e43
halaskar92 <114192735+halaskar92@users.noreply.github.com>
2022-09-23 18:02:30 +0300

    a

--- pwabuilder-adv-sw.js
+++ pwabuilder-adv-sw.js
@@ -1,4 +1,4 @@
 
   import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute';
 
-  precacheAndRoute([{"revision":"43c0adf3faeebc0a8d91eb053ba1d68f","url":"index.html"}]);
+  precacheAndRoute([{"revision":"d41d8cd98f00b204e9800998ecf8427e","url":"index.html"},{"revision":"2fcd9227d0471070a1a77063a1e192c9","url":"sw.js"},{"revision":"2e8108d82eb417009284d8a30b1fd9e1","url":"workbox-926a8ce9.js"},{"revision":"f5bf31f7842b8df1c61244b98a34a398","url":"workbox-config.js"}]);
